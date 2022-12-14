01cefc0f652878056ed3aee3d58ae18206f10441
NicoletteBoggs <nrb1628@gmail.com>
2022-12-13 17:15:07 -0700

    more prompts

--- server.js
+++ server.js
@@ -2,4 +2,51 @@ const db = require("./connection");
 const inquirer = require("inquirer");
 
 // starting prompt to ask qustions
-const initprompt = () => {};
+const initprompt = () => {
+  inquirer
+    .prompt([
+      {
+        name: "prompt",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Create a new department",
+          "Create a new role",
+          "Add a new employee",
+          "Update employee role",
+          "Quit",
+        ],
+      },
+    ])
+    .then(function (res) {
+      switch (res.prompt) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "Create a new department":
+          addDepartment();
+          break;
+        case "Create a new role":
+          addRole();
+          break;
+        case "Add a new employee":
+          addEmployee();
+          break;
+        case "Update employee role":
+          updateRole();
+          break;
+        case "Quit":
+          Quit();
+          break;
+      }
+    });
+};
